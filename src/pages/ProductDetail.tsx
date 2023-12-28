0d3ef95e68361400832001e5f95b46c66e1c83e5
EarthCaspian <hazar.a@outlook.com>
2023-12-28 20:54:36 +0300

    card detail template added

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -2,6 +2,7 @@ import React, { useEffect,useState } from 'react'
 import { useLocation, useParams } from 'react-router-dom'
 import ProductService from '../services/ProductService';
 import { ProductModel } from '../models/responses/ProductModel';
+import { Link } from 'react-router-dom';
 
 type Props = {}
 
@@ -23,7 +24,28 @@ const ProductDetail = (props: Props) => {
     
 
   return (
-    <div>{product?.title}</div>
+    <>
+    <div className='container mt-5 d-flex justify-content-center align-items-center'>
+    <div className="card w-50 text-center">
+  <img src={product?.thumbnail} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{product?.title}</h5>
+    <p className="card-text">{product?.description}</p>
+  </div>
+  <ul className="list-group list-group-flush">
+    <li className="list-group-item">{product?.price}</li>
+    <li className="list-group-item">{product?.brand}</li>
+    <li className="list-group-item">{product?.category}</li>
+  </ul>
+  <div className="card-body">
+    <Link to={"/"} className="card-link">Card link</Link>
+    <Link to={"/"}className="card-link">Another link</Link>
+  </div>
+</div>
+    </div>
+
+    </>
+    
   )
 }
 
