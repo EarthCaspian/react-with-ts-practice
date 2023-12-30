435b3cb3aa19268081ce1d9604c1f53d72a4772f
EarthCaspian <hazar.a@outlook.com>
2023-12-30 13:38:27 +0300

    navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,23 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.css"
 import { Homepage } from './pages/Homepage';
 import ProductDetail from './pages/ProductDetail';
+import Navbar from './components/Navbar/Navbar';
+import ProductAdd from './pages/ProductAdd/ProductAdd';
 
 
 function App(): ReactElement {
-  return <BrowserRouter>
+  return (
+    <>
+    <Navbar/>
     <Routes>
       <Route path='/' element={<Homepage/>}></Route>
       <Route path='/product-detail/:id' element={<ProductDetail/>}></Route>
+      <Route path='/product-add' element={<ProductAdd/>}></Route>
     </Routes>
-  </BrowserRouter>;
+  </>
+  )
+  
+  
 }
 
 export default App;
