0c3620b104b5ab7e75a7fc06a87d9d5cd413211e
EarthCaspian <hazar.a@outlook.com>
2024-01-06 20:24:12 +0300

    formatting

--- src/pages/ProductAdd/ProductAdd.tsx
+++ src/pages/ProductAdd/ProductAdd.tsx
@@ -1,63 +1,74 @@
-import { Formik , Form} from 'formik'
-import React from 'react'
-import * as Yup from 'yup';
-import { passwordRule } from '../../utils/validation/customValidationRules';
-import FormikInput from '../../components/FormikInput/FormikInput';
-import ColorForm from '../../components/ColorForm/ColorForm';
+import { Formik, Form } from "formik";
+import React from "react";
+import * as Yup from "yup";
+import { passwordRule } from "../../utils/validation/customValidationRules";
+import FormikInput from "../../components/FormikInput/FormikInput";
+import ColorForm from "../../components/ColorForm/ColorForm";
 
-type Props = {}
+type Props = {};
 
 interface ProductAddForm {
-    title:string,
-    description:string,
-    price:number,
-    stock:number;
-    colorId:number;
+  title: string;
+  description: string;
+  price: number;
+  stock: number;
+  colorId: number;
 }
 
 const ProductAdd = (props: Props) => {
-    const initialValues: ProductAddForm = {
-        title:"",
-        description:"",
-        price: 0,
-        stock:0,
-        colorId:0,
-    }
+  const initialValues: ProductAddForm = {
+    title: "",
+    description: "",
+    price: 0,
+    stock: 0,
+    colorId: 0,
+  };
 
-const validationSchema = Yup.object({
-    title:Yup.string()
-    .required("Please fill in the title, it's required")
-    .min(2)
-    .max(50)
-    .test("is-strong",
-    "Bu alan en az 1 büyük, 1 küçük harf ve 1 numerik değer içermelidir",
-    passwordRule
-    ),
-    description:Yup.string().required().min(5).max(300),
-    price:Yup.number().min(0),
-    stock:Yup.number().min(0).integer(),
-    colorId: Yup.number().min(1)
-})
+  const validationSchema = Yup.object({
+    title: Yup.string()
+      .required("Please fill in the title, it's required")
+      .min(2)
+      .max(50)
+      .test(
+        "is-strong",
+        "Bu alan en az 1 büyük, 1 küçük harf ve 1 numerik değer içermelidir",
+        passwordRule
+      ),
+    description: Yup.string().required().min(5).max(300),
+    price: Yup.number().min(0),
+    stock: Yup.number().min(0).integer(),
+    colorId: Yup.number().min(1),
+  });
 
   return (
     <div className="container mt-5">
-        <Formik validationSchema={validationSchema} initialValues={initialValues} onSubmit={(values) => {console.log(values);}}>
-            <Form>
-				
-                <FormikInput name='title' label='Title'/>
-                <FormikInput name='description' label='Description'/>
-                <FormikInput name='price' label='Price' type='number'/>
-                <FormikInput name='stock' label='In stock' type='number'/>
-                <ColorForm colors={[{ id: 1, name: 'Red' }, { id: 2, name: 'Black' }, { id: 3, name: 'White' }]}/>
+      <Formik
+        validationSchema={validationSchema}
+        initialValues={initialValues}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        <Form>
+          <FormikInput name="title" label="Title" />
+          <FormikInput name="description" label="Description" />
+          <FormikInput name="price" label="Price" type="number" />
+          <FormikInput name="stock" label="In stock" type="number" />
+          <ColorForm
+            colors={[
+              { id: 1, name: "Red" },
+              { id: 2, name: "Black" },
+              { id: 3, name: "White" },
+            ]}
+          />
 
-				<button type="submit" className="btn btn-primary mt-2">
-					Add Product
-				</button>
-			</Form>
-        </Formik>
-			
-		</div>
-  )
-}
+          <button type="submit" className="btn btn-primary mt-2">
+            Add Product
+          </button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
 
-export default ProductAdd
+export default ProductAdd;
--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,52 +1,52 @@
-import React, { useEffect,useState } from 'react'
-import { useLocation, useParams } from 'react-router-dom'
-import ProductService from '../services/ProductService';
-import { ProductModel } from '../models/responses/ProductModel';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import ProductService from "../services/ProductService";
+import { ProductModel } from "../models/responses/ProductModel";
+import { Link } from "react-router-dom";
 
-type Props = {}
+type Props = {};
 
 const ProductDetail = (props: Props) => {
-  
-    //const location = useLocation();
-    const params = useParams<{id:string}>();
-    const [product, setProduct] = useState<ProductModel>()
+  //const location = useLocation();
+  const params = useParams<{ id: string }>();
+  const [product, setProduct] = useState<ProductModel>();
 
-    useEffect(() => {
-      //let query = new URLSearchParams(location.search)
-      console.log(params);
-      if (params.id) {
-        ProductService.getById(parseInt(params.id)).then(response => {
-            setProduct(response.data)
-        })
-      }
-    }, [])
-    
+  useEffect(() => {
+    //let query = new URLSearchParams(location.search)
+    console.log(params);
+    if (params.id) {
+      ProductService.getById(parseInt(params.id)).then((response) => {
+        setProduct(response.data);
+      });
+    }
+  }, []);
 
   return (
     <>
-    <div className='container mt-5 d-flex justify-content-center align-items-center'>
-    <div className="card w-50 text-center">
-  <img src={product?.thumbnail} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{product?.title}</h5>
-    <p className="card-text">{product?.description}</p>
-  </div>
-  <ul className="list-group list-group-flush">
-    <li className="list-group-item">{product?.price}</li>
-    <li className="list-group-item">{product?.brand}</li>
-    <li className="list-group-item">{product?.category}</li>
-  </ul>
-  <div className="card-body">
-    <Link to={"/"} className="card-link">Card link</Link>
-    <Link to={"/"}className="card-link">Another link</Link>
-  </div>
-</div>
-    </div>
-
+      <div className="container mt-5 d-flex justify-content-center align-items-center">
+        <div className="card w-50 text-center">
+          <img src={product?.thumbnail} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{product?.title}</h5>
+            <p className="card-text">{product?.description}</p>
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">{product?.price}</li>
+            <li className="list-group-item">{product?.brand}</li>
+            <li className="list-group-item">{product?.category}</li>
+          </ul>
+          <div className="card-body">
+            <Link to={"/"} className="card-link">
+              Card link
+            </Link>
+            <Link to={"/"} className="card-link">
+              Another link
+            </Link>
+          </div>
+        </div>
+      </div>
     </>
-    
-  )
-}
+  );
+};
 
-export default ProductDetail
+export default ProductDetail;
