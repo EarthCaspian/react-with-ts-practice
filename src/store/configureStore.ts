08676e1c1cf8ce3ac42930c0883f0e2e8004f1fc
EarthCaspian <hazar.a@outlook.com>
2024-01-08 17:33:36 +0300

    base redux

---
+++ src/store/configureStore.ts
@@ -0,0 +1,13 @@
+//import {legacy_createStore as createStore} from "redux";
+import { cartSliceReducer } from "./rootReducer";
+import { configureStore } from "@reduxjs/toolkit";
+
+// export const configureStore = () => {
+//   return createStore(rootReducer);
+// };
+
+export default configureStore({
+  reducer: {
+    createStore: cartSliceReducer,
+  },
+});
---
+++ src/store/reducers/cartReducer.ts
@@ -0,0 +1,23 @@
+const initialState = {
+    cartItems: [],
+}
+
+export const cartReducer = (state:any = initialState,action:any) => {
+    // switch-case
+    switch(action.type)
+    {
+        case "ADD_TO_CART":
+            // State is immutable..
+            // state.cartItems.push(action.payload) X
+            return { ...state, cartItems:[...state.cartItems, action.payload]}
+            
+        case "REMOVE_FROM_CART":
+            return { ...state, cartItems:[...state.cartItems.filter(
+                (i:any) => i.id !== action.payload.id)
+            ]}
+        case "CLEAR_CART":
+            return {...state, cartItems: [] }
+        default:
+            return state;
+    }
+}
