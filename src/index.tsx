8fc552b92c3c2d9d4155598e0f1e59ce56090f91
EarthCaspian <hazar.a@outlook.com>
2024-01-12 16:53:55 +0300

    toastr css

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/reducksTKStore"
 import { AuthProvider } from "./contexts/AuthContext";
+import "toastr/build/toastr.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
